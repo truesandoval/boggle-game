54c888ff68e9d634625cd4506c18f6c65a87316c
truesandoval <44210761+truesandoval@users.noreply.github.com>
2020-01-27 22:55:07 -0800

    grid
    rough grid implementation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Grid from './components/grid';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Boggle 
         </p>
+        <Grid>
+        </Grid>
         <a
           className="App-link"
           href="https://reactjs.org"
